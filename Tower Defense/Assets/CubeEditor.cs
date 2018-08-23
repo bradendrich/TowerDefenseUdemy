62a62b6f8d52fd14d722fdc6cb66f957611eb257
Braden R <bradendrich@hotmail.com>
2018-08-23 14:50:05 -0600

    Renamed Script

--- Tower Defense/Assets/CubeEditor.cs
+++ Tower Defense/Assets/CubeEditor.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class SnapPosition : MonoBehaviour {
+public class CubeEditor : MonoBehaviour
+{
 
     [SerializeField] [Range (1f, 20f)] float gridSize = 10f;
 
-	void Update () {
+    TextMesh textMesh;
+
+    private void Start()
+    {
+        
+    }
+
+    void Update ()
+    {
         Vector3 snapPos;
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
