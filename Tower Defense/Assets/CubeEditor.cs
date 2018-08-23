910e59a2130c44922550fef00e6cf65d8e8ef0ff
Braden R <bradendrich@hotmail.com>
2018-08-23 15:02:14 -0600

    Labeled Cubes
    Labeled the cubes

--- Tower Defense/Assets/CubeEditor.cs
+++ Tower Defense/Assets/CubeEditor.cs
@@ -3,24 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
+[SelectionBase]
 public class CubeEditor : MonoBehaviour
 {
 
     [SerializeField] [Range (1f, 20f)] float gridSize = 10f;
 
     TextMesh textMesh;
-
-    private void Start()
-    {
-        
-    }
+    Vector3 snapPos;
 
     void Update ()
     {
-        Vector3 snapPos;
+        UpdateTextOnCube();
+
+        SnapCubePosition();
+    }
+
+    private void SnapCubePosition()
+    {
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
 
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
-	}
+    }
+
+    private void UpdateTextOnCube()
+    {
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = snapPos.x / gridSize + "," + snapPos.z / gridSize;
+    }
 }
