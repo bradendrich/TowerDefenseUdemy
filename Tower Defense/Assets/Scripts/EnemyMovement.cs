f5eb8407acff38b6c68814a95ff5e184b10bd151
Braden R <bradendrich@hotmail.com>
2018-08-23 15:42:03 -0600

    Prints Pathway
    Prints the Path to the Console

--- Tower Defense/Assets/Scripts/EnemyMovement.cs
+++ Tower Defense/Assets/Scripts/EnemyMovement.cs
@@ -2,11 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMovement : MonoBehaviour {
+public class EnemyMovement : MonoBehaviour
+{
 
     [SerializeField] List<Block> path;
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Start()
+    {
+        PrintAllWaypoints();
+    }
+
+    private void PrintAllWaypoints()
+    {
+        foreach (Block waypoint in path)
+        {
+            print(waypoint);
+        }
+    }
+
+    void Update ()
+    {
 		
 	}
 }
