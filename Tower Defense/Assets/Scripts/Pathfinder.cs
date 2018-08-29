8bb9133cd6aac9fbcd2784635fc9fd851554baff
Braden R <bradendrich@hotmail.com>
2018-08-29 08:46:12 -0600

    Initial Queue
    Started a queue

--- Tower Defense/Assets/Scripts/Pathfinder.cs
+++ Tower Defense/Assets/Scripts/Pathfinder.cs
@@ -5,15 +5,40 @@ using UnityEngine;
 
 public class Pathfinder : MonoBehaviour
 {
-    // I am really exited to learn Pathfinding!
     Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    Queue<Waypoint> queue = new Queue<Waypoint>();
+
     [SerializeField] Waypoint startWaypoint, endWaypoint;
     Vector2Int[] directions = { Vector2Int.up, Vector2Int.right, Vector2Int.down, Vector2Int.left };
+
+    bool isRunning = true;
     void Start ()
     {
         LoadBlocks();
         ColorStartAndEnd();
-        ExploreNeighbors();
+        FindingPath();
+        //ExploreNeighbors();
+    }
+
+    private void FindingPath()
+    {
+        queue.Enqueue(startWaypoint);
+        while (queue.Count > 0)
+        {
+            var searchCenter = queue.Dequeue();
+            print("Searching from: " + searchCenter); //ToDo remove log
+            SearchingFromEndPoint(searchCenter);
+        }
+        print("Finished Pathfinding?");
+    }
+
+    private void SearchingFromEndPoint(Waypoint searchCenter)
+    {
+        if (searchCenter == endWaypoint)
+        {
+            print("You found the endpoint, stopping search"); //Todo Remove Log
+            isRunning = false;
+        }
     }
 
     private void ExploreNeighbors()
