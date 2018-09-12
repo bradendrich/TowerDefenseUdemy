e6b4a30774c290978a28540d6c4dae7f1ea95fe6
Braden R <bradendrich@hotmail.com>
2018-09-11 21:53:49 -0600

    Adjusted the position
    Adjusted the position of the enemy

--- Tower Defense/Assets/Scripts/EnemyMovement.cs
+++ Tower Defense/Assets/Scripts/EnemyMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    //[SerializeField] Transform offsetPosition;
     void Start()
     {
         Pathfinder pathfinder = FindObjectOfType<Pathfinder>();
@@ -13,11 +14,10 @@ public class EnemyMovement : MonoBehaviour
 
     IEnumerator FollowPath(List<Waypoint> path)
     {
-        var currentPosition = this.transform.position;
         print("Starting Patrol...");
         foreach (Waypoint waypoint in path)
         {
-            transform.position = waypoint.transform.position + currentPosition;
+            transform.position = waypoint.transform.position + new Vector3(0, 0, 5);
             yield return new WaitForSeconds(1f);
         }
         print("Patrol Finished");
