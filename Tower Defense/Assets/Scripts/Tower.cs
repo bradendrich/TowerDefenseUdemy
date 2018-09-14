acd21c128e374113c1403fc36ce7db40ddb7c854
Braden R <bradendrich@hotmail.com>
2018-09-13 21:19:07 -0600

    Tower Script

---
+++ Tower Defense/Assets/Scripts/Tower.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] Transform MoveToPan;
+
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+}
